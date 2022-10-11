992be079b06ececaee553997dabc3b1aa787a931
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-11 14:49:30 +0600

    :sparkles: first task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const randomBodyColorHandler = () => body.style.backgroundColor = `${getRandomHexColor()}`;
+
+let interval = '';
+stopBtn.setAttribute('disabled', 'disabled');
+
+function startBtnHandler() {
+  interval = setInterval(randomBodyColorHandler, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+}
+
+
+function stopBtnHandler() {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+}
+
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
