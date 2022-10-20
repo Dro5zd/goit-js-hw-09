ff920a72fa1d29b626a8a1603605ca6a60aa7689
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-21 00:05:03 +0600

    :bug: refactor promise part

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,49 +10,28 @@ form.addEventListener('input', e => {
 form.addEventListener('submit', (event) => {
   let { step, amount, delay } = obj;
   event.preventDefault();
-  let num = 1;
-  let newDelay = +delay - +step;
-
-  let interval = setTimeout(() => {
-    newDelay += +step;
-    createPromise(num, newDelay)
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-
-    interval = setInterval(() => {
-      num += 1;
-      newDelay += +step;
-      createPromise(num, newDelay)
-        .then(({ position, delay }) => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-      if (num === +amount) {
-        clearInterval(interval);
-        document.querySelectorAll('input').forEach(el => el.value = '');
-      }
-    }, +step);
-  }, delay);
-
+    delay = +delay + +step
+  }
+  form.reset();
 });
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
-
-
-
-
